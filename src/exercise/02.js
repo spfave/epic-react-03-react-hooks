af18e86c9b0c33ea9674d9e4fe8761cc613b7d14
Sebastian F <sebpfdev@gmail.com>
2022-03-23 23:12:54 -0400

    Complete exercise 2

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -3,14 +3,46 @@
 
 import * as React from 'react'
 
+// Extra credit 3: create custom hook to use local storage
+// Extra credit 4: generalize hook by serializing value when storing
+function useLocalStorageState(
+  storageKey,
+  initialValue,
+  {serialize = JSON.stringify, deserialize = JSON.parse} = {},
+) {
+  const [state, setState] = React.useState(() => {
+    const lsValue = localStorage.getItem(storageKey)
+    if (lsValue) return deserialize(lsValue)
+    return typeof initialValue === 'function' ? initialValue() : initialValue
+  })
+
+  React.useEffect(() => {
+    window.localStorage.setItem(storageKey, serialize(state))
+  }, [storageKey, state, serialize])
+
+  return [state, setState]
+}
+
 function Greeting({initialName = ''}) {
   // 🐨 initialize the state to the value from localStorage
   // 💰 window.localStorage.getItem('name') ?? initialName
-  const [name, setName] = React.useState(initialName)
+
+  // Extra credit 1: lazy state initialization
+  // const [name, setName] = React.useState(
+  //   () => window.localStorage.getItem('name') ?? initialName,
+  // )
 
   // 🐨 Here's where you'll use `React.useEffect`.
   // The callback should set the `name` in localStorage.
   // 💰 window.localStorage.setItem('name', name)
+
+  // Extra credit 2: add useEffect dependency array
+  // React.useEffect(() => {
+  //   window.localStorage.setItem('name', name)
+  // }, [name])
+
+  // Extra credit 3: use custom hook
+  const [name, setName] = useLocalStorageState(initialName, 'name')
 
   function handleChange(event) {
     setName(event.target.value)
@@ -19,7 +51,7 @@ function Greeting({initialName = ''}) {
     <div>
       <form>
         <label htmlFor="name">Name: </label>
-        <input value={name} onChange={handleChange} id="name" />
+        <input id="name" value={name} onChange={handleChange} />
       </form>
       {name ? <strong>Hello {name}</strong> : 'Please type your name'}
     </div>
--- survey-notes.md
+++ survey-notes.md
@@ -6,3 +6,12 @@ Input to useState hook: initial state variable value
 Return values of useState hook: 
 1. state value variable 
 2. state setter function (used to trigger a UI rerender, displaying a state variable change)  
+
+## 02: useEffect
+useEffect hook is used to run side effects  
+Input to useEffect hook: 
+1. a execution function to perform the side effect
+2. a list of dependencies to watch for changes to invoke the execution function
+
+Passing a function as the initial value to useState performs "lazy" initialization - function will only be executed once on initial render  
+Creation of custom hooks - functions that use hooks to implement logic  
